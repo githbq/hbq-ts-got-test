2a2f87909dcec0e62c5b576694391c816f946452
biqing.hu@okg.com <biqing.hu@okg.com>
2022-11-26 11:50:05 +0800

    ok

--- run.js
+++ run.js
@@ -1,6 +1,6 @@
- require('ts-node/register')
+ import('ts-node/register')
 
- require('./src/index.ts')
+ import('./src/index.ts')
      //tsconfig 帮助文档地址
      //https://zhongsp.gitbooks.io/typescript-handbook/content/doc/handbook/tsconfig.json.html
      //https://tslang.cn/docs/handbook/compiler-options.html
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
+import got from 'got'
+import * as http from 'http'
 /**
  * entry
  */
+let count = 0
 !(async () => {
-    debugger
-    console.log('hello typescript')
+    http.createServer(async function (req, res) {
+        console.log("ts count", ++count);
+
+        const map = new Map();
+        debugger;
+        const response = await got("http://127.0.0.1:8000", { cache: map });
+        console.log("1.response.isFromCache", response.isFromCache);
+
+        const response2 = await got("http://127.0.0.1:8000", { cache: map });
+        console.log("2.response.isFromCache", response2.isFromCache);
+
+
+        res.end('hello world');
+    })
+        .listen(6000);
+    console.log("http://127.0.0.1:6000");
+
 })()
 
 export const add = (a, b) => a + b
